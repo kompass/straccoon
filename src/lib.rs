7caea4918b52b473f953c6b586a16090c28c4343
Nicolas Kirchner <kirchner.nic@gmail.com>
2019-12-15 22:38:07 +0100

    Add byte() and bytes()

--- src/lib.rs
+++ src/lib.rs
@@ -147,17 +147,17 @@ impl<S: Streamer, F: FnMut(u8) -> bool> Parser for Byte<S, F> {
 }
 
 
-pub fn byte<S: Streamer, F: FnMut(u8) -> bool>(predicate: F) -> Byte<S, F> {
+pub fn byte_predicate<S: Streamer, F: FnMut(u8) -> bool>(predicate: F) -> Byte<S, F> {
     Byte(predicate, PhantomData)
 }
 
 
 macro_rules! ascii_predicate_parser {
     ($name:ident, $f: ident) => {{
-        byte(|c: u8| AsciiChar::from_ascii(c).map(|c| c.$f()).unwrap_or(false))
+        byte_predicate(|c: u8| AsciiChar::from_ascii(c).map(|c| c.$f()).unwrap_or(false))
     }};
     ($name:ident, $f: ident $($args:tt)+) => {{
-        byte(|c: u8| AsciiChar::from_ascii(c).map(|c| c.$f $($args)+).unwrap_or(false))
+        byte_predicate(|c: u8| AsciiChar::from_ascii(c).map(|c| c.$f $($args)+).unwrap_or(false))
     }};
 }
 
@@ -179,6 +179,34 @@ pub fn letter<S: Streamer>() -> Byte<S, impl FnMut(u8) -> bool> {
 
 pub fn space<S: Streamer>() -> Byte<S, impl FnMut(u8) -> bool> {
     ascii_predicate_parser!(space, is_ascii_whitespace)
+}
+
+
+pub fn byte<S: Streamer>(b: u8) -> Byte<S, impl FnMut(u8) -> bool> {
+    byte_predicate(move |c: u8| c == b)
+}
+
+
+pub struct Bytes<S: Streamer>(&'static [u8], PhantomData<S>);
+
+
+impl<S: Streamer> Parser for Bytes<S> {
+    type Input = S;
+
+    fn parse(&mut self, stream: &mut S) -> Result<(), ParserError> {
+        assert!(self.0.len() > 0);
+
+        for b in self.0 {
+            byte_predicate(move |c: u8| c == *b).parse(stream)?;
+        }
+
+        Ok(())
+    }
+}
+
+
+pub fn bytes<S: Streamer>(range: &'static [u8]) -> Bytes<S> {
+    Bytes(range, PhantomData)
 }
 
 
@@ -481,18 +509,32 @@ mod tests {
 
 
     #[test]
+    fn it_parses_a_precise_byte() {
+        let fake_read = &b"This is the text !"[..];
+        let mut stream = ElasticBufferStreamer::unlimited(fake_read);
+
+        let mut parser = byte(b'T');
+
+        assert!(parser.parse(&mut stream).is_ok());
+        assert_eq!(stream.position(), 1);
+    }
+
+
+    #[test]
     fn it_parses_alpha_num() {
         let fake_read1 = &b"!This is the text !"[..];
         let mut stream1 = ElasticBufferStreamer::unlimited(fake_read1);
 
         let mut parser1 = alpha_num();
         assert!(parser1.parse(&mut stream1).is_err());
+        assert_eq!(stream1.position(), 0);
 
         let fake_read2 = &b"This is the text !"[..];
         let mut stream2 = ElasticBufferStreamer::unlimited(fake_read2);
 
         let mut parser2 = alpha_num();
         assert!(parser2.parse(&mut stream2).is_ok());
+        assert_eq!(stream2.position(), 1);
     }
 
     #[test]
@@ -515,6 +557,19 @@ mod tests {
         let mut stream = ElasticBufferStreamer::unlimited(fake_read);
 
         let mut parser = many(alpha_num());
+
+        let rg = parser.get(&mut stream).unwrap();
+
+        assert_eq!(rg, &(b"This")[..]);
+    }
+
+
+    #[test]
+    fn it_gets_a_precise_range() {
+        let fake_read = &b"This is the text !"[..];
+        let mut stream = ElasticBufferStreamer::unlimited(fake_read);
+
+        let mut parser = bytes(&b"This"[..]);
 
         let rg = parser.get(&mut stream).unwrap();
 
